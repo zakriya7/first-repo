55770f28dbb0bad4533b336a8d2251a5a98d6b79
talhakhannnnn <muhammadtalhakhan1@gmail.com>
2018-11-22 13:37:59 +0500

    Add files via upload

---
+++ src/main/java/com/jenkinstask/sumfunction/Input.java
@@ -0,0 +1,31 @@
+package com.northbaysolutions.sumfunction;
+
+public class Input {
+
+    private Integer num1;
+    private Integer num2;
+
+    public Input() {
+    }
+
+    public Input(Integer num1, Integer num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public Integer getNum1() {
+        return num1;
+    }
+
+    public void setNum1(Integer num1) {
+        this.num1 = num1;
+    }
+
+    public Integer getNum2() {
+        return num2;
+    }
+
+    public void setNum2(Integer num2) {
+        this.num2 = num2;
+    }
+}
